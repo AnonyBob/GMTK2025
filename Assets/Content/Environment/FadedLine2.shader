4e193dfa619e5933d24900f0b17f0dc44c35c220
Nicholas Stringer <nicholas.stri@gmail.com>
2025-08-01 19:54:20 -0500

    Adding the path and better floors.

---
+++ Assets/Content/Environment/FadedLine2.shader
@@ -0,0 +1,72 @@
+Shader "Custom/SpriteLitLineRendererFade"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Color ("Tint", Color) = (1,1,1,1)
+        _Amount ("Amount", Range(0, 1)) = 1
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue"="Transparent" "RenderPipeline"="UniversalPipeline" }
+        LOD 100
+        Blend SrcAlpha OneMinusSrcAlpha
+        ZWrite Off
+        Cull Off
+
+        Pass
+        {
+            Name "ForwardLit"
+            Tags { "LightMode" = "Universal2D" }
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float4 color : COLOR;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _Color;
+            float _Amount;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex.xyz);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.color = v.color * _Color;
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                float2 uv = i.uv;
+
+                // Edge fade based on horizontal UV (U coordinate)
+                float fade = smoothstep(0.0, _Amount, uv.y) * smoothstep(1.0, 1.0 - _Amount, uv.y);
+
+                float4 texColor = tex2D(_MainTex, uv) * i.color;
+                texColor.a *= fade;
+
+                return texColor;
+            }
+            ENDHLSL
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DefaultNamespace.Towers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
@@ -17,13 +18,23 @@ namespace DefaultNamespace
         public int RunCost;
     }
     
-    public class Tower : MonoBehaviour
+    public enum PlacementState
+    {
+        Placing,
+        Placed,
+        Selected
+    }
+    
+    public class Tower : MonoBehaviour, IPointerClickHandler
     {
         [field: SerializeField]
         public TowerStats Stats { get; private set; }
         
         [field: SerializeField]
         public Tower Upgrade { get; private set; }
+
+        [SerializeField]
+        private Animator _animator;
         
         [SerializeField]
         private int _attempts = 10;
@@ -31,6 +42,7 @@ namespace DefaultNamespace
         private readonly List<Walker> _targets = new List<Walker>();
         private float _timeSinceLastTick;
         private EffectPool _effectPool;
+        private PlacementState _placementState;
 
         protected EffectPool EffectPool
         {
@@ -43,9 +55,46 @@ namespace DefaultNamespace
                 return _effectPool;
             }
         }
+
+        public void PrepareForPlacing()
+        {
+            _placementState = PlacementState.Placing;
+            _animator.SetBool("Placing", true);
+            _animator.SetBool("Selected", false);
+        }
+
+        public void Place()
+        {
+            _placementState = PlacementState.Placed;
+            _animator.SetBool("Placing", false);
+            _animator.SetBool("Selected", false);
+        }
+
+        public void Select()
+        {
+            _placementState = PlacementState.Selected;
+            _animator.SetBool("Placing", false);
+            _animator.SetBool("Selected", true);
+            
+            //TODO: Show Options....
+        }
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (_placementState == PlacementState.Placed) {
+                Select();
+            }
+            else if (_placementState == PlacementState.Selected) {
+                Place();
+            }
+        }
         
         private void Update()
         {
+            if (_placementState == PlacementState.Placing) {
+                return;
+            }
+            
             _timeSinceLastTick += Time.deltaTime;
             if (_timeSinceLastTick >= Stats.TickRate) {
                 Fire();
